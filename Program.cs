cb971f02c32e555d3454895d033fb7d00a6f397e
Revan Arturito <revan.arturito@gmail.com>
2024-02-27 15:06:39 +0700

    Menambahkan nama mahasiswa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Masukkan nama anda : ");
+string nama = Console.ReadLine();
+Console.WriteLine($"Hello {nama}!, km pcar imel");
+
